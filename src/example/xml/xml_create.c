25eea136ce3c02059a3f966fffef62ebee5337df
Christophe Enderlin <c.enderlin@me.com>
2014-12-29 23:30:59 +0100

    .

--- src/example/xml/xml_create.c
+++ src/example/xml/xml_create.c
@@ -9,10 +9,11 @@ int main(int argc, char **argv) {
     xmlDocPtr doc;
     xmlNodePtr element, temp;
     xmlAttrPtr newattr;
-    xmlChar *a, *b, *height;
+    xmlChar *a, *b, *height, *id, *stage, *member;
     a = (xmlChar*)"3";
     b = (xmlChar*)"6";
     height = (xmlChar*)"3";
+
 
     if (argc <= 1) {
         printf("Usage: %s docname\n", argv[0]);
@@ -26,10 +27,29 @@ int main(int argc, char **argv) {
 
     if (doc != NULL) {
 
+        // root <dst>
         element = xmlNewDocNode(doc, NULL, (const xmlChar*)"dst", NULL);
         newattr = xmlNewProp(element, (const xmlChar*)"a", a);
+        newattr = xmlNewProp(element, (const xmlChar*)"b", b);
+        newattr = xmlNewProp(element, (const xmlChar*)"height", height);
         temp = xmlDocSetRootElement(doc, element);
 
+        // children <node>
+        id = (xmlChar*)"42";
+        element = xmlNewTextChild(element, NULL, (const xmlChar*)"node", NULL);
+        newattr = xmlNewProp(element, (const xmlChar*)"id", id);
+
+        // children <stage>
+        stage = (xmlChar*)"0";
+        element = xmlNewTextChild(element, NULL, (const xmlChar*)"stage", NULL);
+        newattr = xmlNewProp(element, (const xmlChar*)"value", stage);
+
+        // children <member>
+        member = (xmlChar*)"42";
+        element = xmlNewTextChild(element, NULL, (const xmlChar*)"member", NULL);
+        newattr = xmlNewProp(element, (const xmlChar*)"value", member);
+
+        // save doc to disk
         xmlSaveFormatFile (docname, doc, 0);
         xmlFreeDoc(doc);
     }
